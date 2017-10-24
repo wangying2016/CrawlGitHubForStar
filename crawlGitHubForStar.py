0c47896152ed82333b2e76b4f67b096e0c41e9ab
Wang Ying <wy867504146@outlook.com>
2017-10-24 21:09:41 +0800

    Crawl GitHub For Stars.

---
+++ crawlGitHubForStar.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+userName = input()
+url = userName + '?tab=repositories'
+openFailed = False
+count = 0
+
+while True:
+    # 1. Open repositories page.
+    try:
+        html = urlopen('https://github.com/' + url)
+        bsObj = BeautifulSoup(html)
+    except HTTPError as e:
+        print('open ' + 'https://github.com/' + url + ' failed.')
+        openFailed = True
+        break
+
+    # 2. Count stars at one page.
+    for star in bsObj.findAll('svg', {'aria-label': 'star'}):
+        count += int(star.parent.get_text().replace(',', ''))
+
+    # 3. Find next page.
+    nextPage = bsObj.find('a', {'class': 'next_page'})
+    if nextPage is None:
+        break
+    else:
+        url = nextPage.attrs['href']
+
+if openFailed is False:
+    print(userName + ' has ' + str(count) + ' stars.')
+
+
